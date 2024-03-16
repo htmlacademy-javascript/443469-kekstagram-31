360695c1960730245d45bad3ce4018add5f4aead
Keks <keks@htmlacademy.ru>
2024-03-17 00:37:49 +0300

    :heavy_check_mark: Сборка #9

--- 9/js/main.js
+++ 9/js/main.js
@@ -1,7 +1,6 @@
 import {NUMBER_OBJECTS} from './mocks/consts.js';
 import {generatePublishedPhotos} from './mocks/content.js';
 import {createThumbnails} from './create-photos.js';
-import './generate-content-popup.js';
 
 const getPublishedPhotos = generatePublishedPhotos(NUMBER_OBJECTS);
 
