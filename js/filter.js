5c08c45b927e643471632136ff8a93ee365c7f9e
Tanya <tanyapotapchik@gmai.com>
2024-03-30 22:05:42 +0100

    debounce working func

--- js/filter.js
+++ js/filter.js
@@ -3,40 +3,71 @@ import {createThumbnails} from './create-photos.js';
 
 const MAX_PHOTO_COUNT = 10;
 const ACTIVE_CLASS = 'img-filters__button--active';
+
+const MenuFilter = {
+  DEFAULT: 'DEFAULT',
+  RANDOM: 'RANDOM',
+  DISCUSSED: 'DISCUSSED'
+};
+
 const filtersEl = document.querySelector('.img-filters__form');
+const defaultFilterEl = document.querySelector('#filter-default');
+const randomFilterEl = document.querySelector('#filter-random');
+const discussedFilterEl = document.querySelector('#filter-discussed');
+
+let activeFilterButton = null;
+let filterButtonLoaded = null;
 let photos = [];
 
 const clearPhotos = () => {
   document.querySelectorAll('a.picture').forEach((photo) => photo.remove());
 };
 
-const showDefaultPhotos = () => {
-  clearPhotos();
-  createThumbnails(photos);
-};
-
-const showRandomPhotos = () => {
-  clearPhotos();
-  createThumbnails(photos.slice().sort(() => 0.5 - Math.random()).slice(0, MAX_PHOTO_COUNT));
-
-};
-
-const showDiscussedPhotos = () => {
-  clearPhotos();
-  createThumbnails(photos.slice().sort((a, b) => b.comments.length - a.comments.length));
-
-};
-
 const onChangeFilter = (evt) => {
   const target = evt.target.closest('button');
   const activeButtonEl = document.querySelector(`.${ACTIVE_CLASS}`);
 
-  if (target === activeButtonEl) {
+  if (target && target === activeButtonEl) {
     return;
   }
-  if (!target.classList.contains(ACTIVE_CLASS)) {
+
+  if (target && !target.classList.contains(ACTIVE_CLASS)) {
     activeButtonEl.classList.toggle(ACTIVE_CLASS);
     target.classList.toggle(ACTIVE_CLASS);
+  }
+};
+
+const loadData = (data, clickedFilterButton) => {
+  if (filterButtonLoaded !== clickedFilterButton) {
+    filterButtonLoaded = clickedFilterButton;
+    clearPhotos();
+    createThumbnails(data);
+  }
+};
+
+const debounceRender = debounce(loadData);
+
+const handleMenuItemClick = (clickedFilterButton) => {
+  if (activeFilterButton !== clickedFilterButton) {
+    activeFilterButton = clickedFilterButton;
+
+    let thumbnailsData;
+
+    switch (clickedFilterButton) {
+      case MenuFilter.DEFAULT:
+        thumbnailsData = photos;
+        break;
+      case MenuFilter.RANDOM:
+        thumbnailsData = photos.slice().sort(() => 0.5 - Math.random()).slice(0, MAX_PHOTO_COUNT);
+        break;
+      case MenuFilter.DISCUSSED:
+        thumbnailsData = photos.slice().sort((a, b) => b.comments.length - a.comments.length);
+        break;
+      default:
+        throw new Error('Unsupported type: ' + clickedFilterButton);
+    }
+
+    debounceRender(thumbnailsData, clickedFilterButton);
   }
 };
 
@@ -46,11 +77,11 @@ const initThumbnails = (photosServer) => {
 
   filtersEl.addEventListener('click', onChangeFilter);
 
-  document.querySelector('#filter-default').addEventListener('click', debounce(showDefaultPhotos));
-  document.querySelector('#filter-random').addEventListener('click', debounce(showRandomPhotos));
-  document.querySelector('#filter-discussed').addEventListener('click', debounce(showDiscussedPhotos));
+  defaultFilterEl.addEventListener('click', () => handleMenuItemClick(MenuFilter.DEFAULT));
+  randomFilterEl.addEventListener('click', () => handleMenuItemClick(MenuFilter.RANDOM));
+  discussedFilterEl.addEventListener('click', () => handleMenuItemClick(MenuFilter.DISCUSSED));
 
-  showDefaultPhotos();
+  handleMenuItemClick(MenuFilter.DEFAULT);
 };
 
 export {initThumbnails};
