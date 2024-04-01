4c2890fbb18643892b35dfb323da1967872d10f6
Keks <keks@htmlacademy.ru>
2024-04-01 19:18:29 +0300

    :heavy_check_mark: Сборка #14

---
+++ 14/js/create-photos.js
@@ -0,0 +1,32 @@
+import {openPopup} from './popup.js';
+
+const photoTemplateEl = document.querySelector('#picture').content.querySelector('.picture');
+const photoFragment = document.createDocumentFragment();
+const photoResultContainerEl = document.querySelector('.pictures');
+
+const createPhotoThumb = ({url, description, likes, comments}) => {
+  const imageThumbnail = photoTemplateEl.cloneNode(true);
+  const imageEl = imageThumbnail.querySelector('.picture__img');
+
+  imageEl.src = url;
+  imageEl.alt = description;
+  imageThumbnail.querySelector('.picture__likes').textContent = likes;
+  imageThumbnail.querySelector('.picture__comments').textContent = comments.length;
+
+  return imageThumbnail;
+};
+
+const createThumbnails = (thumbnails) => {
+  thumbnails.forEach((photo) => {
+    const imageThumbnail = createPhotoThumb(photo);
+    imageThumbnail.addEventListener('click', (evt) => {
+      evt.preventDefault();
+      openPopup(photo);
+    });
+    photoFragment.appendChild(imageThumbnail);
+  });
+
+  photoResultContainerEl.appendChild(photoFragment);
+};
+
+export {createThumbnails};
