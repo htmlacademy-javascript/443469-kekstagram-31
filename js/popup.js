06f7d937ad069f522a3affe957bfba0f53a8f8c8
Tanya <tanyapotapchik@gmai.com>
2024-04-01 23:41:03 +0200

    comments rendering by part

--- js/popup.js
+++ js/popup.js
@@ -1,33 +1,11 @@
 import {isEscapeKey, onEscKeyDown} from './util.js';
-
-const MAX_COMMENTS_COUNT = 5;
-let currentCount = 0;
+import {clearComments, renderComments} from './comments.js';
 
 const popupEl = document.querySelector('.big-picture');
-const commentCountEl = document.querySelector('.social__comment-count');
-const commentShownCountEl = document.querySelector('.social__comment-shown-count');
-const commentTotalCountEl = document.querySelector('.social__comment-total-count');
 const commentLoaderEl = document.querySelector('.comments-loader');
 const commentInputEl = document.querySelector('.social__footer-text');
-
 const bigImageEl = document.querySelector('.big-picture');
 const closeBtnEl = document.querySelector('.big-picture__cancel');
-const commentsEl = bigImageEl.querySelector('.social__comments');
-const commentEl = bigImageEl.querySelector('.social__comment');
-
-const fillComments = (photoComments) => {
-  commentsEl.innerHTML = '';
-  currentCount = 0;
-
-  photoComments.forEach(({avatar, name, message}) => {
-    const newComment = commentEl.cloneNode(true);
-    const picture = newComment.querySelector('.social__picture');
-    picture.src = avatar;
-    picture.alt = name;
-    newComment.querySelector('.social__text').textContent = message;
-    commentsEl.append(newComment);
-  });
-};
 
 const fillPopup = ({url, description, likes, comments}) => {
   const imgEl = bigImageEl.querySelector('.big-picture__img img');
@@ -36,19 +14,7 @@ const fillPopup = ({url, description, likes, comments}) => {
   bigImageEl.querySelector('.social__caption').textContent = description;
   bigImageEl.querySelector('.likes-count').textContent = likes;
 
-  const commentsLength = comments.length;
-  const shownComment = comments.slice(currentCount, currentCount + MAX_COMMENTS_COUNT);
-  const renderedCommentsLength = shownComment.length + currentCount;
-
-  if (commentsLength <= renderedCommentsLength) {
-    commentLoaderEl.classList.add('hidden'); //ok
-  }
-
-  bigImageEl.querySelector('.social__comment-shown-count').textContent = renderedCommentsLength; //ok
-  bigImageEl.querySelector('.social__comment-total-count').textContent = comments.length;//ok
-  currentCount += MAX_COMMENTS_COUNT;
-
-  fillComments(shownComment);
+  renderComments(comments);
 };
 
 closeBtnEl.addEventListener('click', (evt) => {
@@ -76,7 +42,7 @@ const openPopup = (photo) => {
 function closePopup() {
   popupEl.classList.add('hidden');
   commentLoaderEl.classList.remove('hidden');
-
+  clearComments();
   document.removeEventListener('keydown', onPopupEscKeydown);
 }
 
