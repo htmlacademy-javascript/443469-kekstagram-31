feee5d8cdea55d3ee03713a53197fd406a14207a
Tanya <tanyapotapchik@gmai.com>
2024-04-01 22:43:02 +0200

    counter and esc

--- js/popup.js
+++ js/popup.js
@@ -1,8 +1,14 @@
-import {onEscKeyDown} from './util.js';
+import {isEscapeKey, onEscKeyDown} from './util.js';
+
+const MAX_COMMENTS_COUNT = 5;
+let currentCount = 0;
 
 const popupEl = document.querySelector('.big-picture');
 const commentCountEl = document.querySelector('.social__comment-count');
+const commentShownCountEl = document.querySelector('.social__comment-shown-count');
+const commentTotalCountEl = document.querySelector('.social__comment-total-count');
 const commentLoaderEl = document.querySelector('.comments-loader');
+const commentInputEl = document.querySelector('.social__footer-text');
 
 const bigImageEl = document.querySelector('.big-picture');
 const closeBtnEl = document.querySelector('.big-picture__cancel');
@@ -11,6 +17,7 @@ const commentEl = bigImageEl.querySelector('.social__comment');
 
 const fillComments = (photoComments) => {
   commentsEl.innerHTML = '';
+  currentCount = 0;
 
   photoComments.forEach(({avatar, name, message}) => {
     const newComment = commentEl.cloneNode(true);
@@ -28,9 +35,20 @@ const fillPopup = ({url, description, likes, comments}) => {
   imgEl.alt = description;
   bigImageEl.querySelector('.social__caption').textContent = description;
   bigImageEl.querySelector('.likes-count').textContent = likes;
-  bigImageEl.querySelector('.social__comment-total-count').textContent = comments.length;
 
-  fillComments(comments);
+  const commentsLength = comments.length;
+  const shownComment = comments.slice(currentCount, currentCount + MAX_COMMENTS_COUNT);
+  const renderedCommentsLength = shownComment.length + currentCount;
+
+  if (commentsLength <= renderedCommentsLength) {
+    commentLoaderEl.classList.add('hidden'); //ok
+  }
+
+  bigImageEl.querySelector('.social__comment-shown-count').textContent = renderedCommentsLength; //ok
+  bigImageEl.querySelector('.social__comment-total-count').textContent = comments.length;//ok
+  currentCount += MAX_COMMENTS_COUNT;
+
+  fillComments(shownComment);
 };
 
 closeBtnEl.addEventListener('click', (evt) => {
@@ -44,8 +62,12 @@ const openPopup = (photo) => {
   fillPopup(photo);
 
   popupEl.classList.remove('hidden');
-  commentCountEl.classList.add('hidden');
-  commentLoaderEl.classList.add('hidden');
+
+  commentInputEl.addEventListener('keydown', (evt) => {
+    if (isEscapeKey(evt)) {
+      evt.stopPropagation();
+    }
+  });
 
   document.addEventListener('keydown', onPopupEscKeydown);
 };
@@ -53,7 +75,6 @@ const openPopup = (photo) => {
 //function declaration to call it anywhere
 function closePopup() {
   popupEl.classList.add('hidden');
-  commentCountEl.classList.remove('hidden');
   commentLoaderEl.classList.remove('hidden');
 
   document.removeEventListener('keydown', onPopupEscKeydown);
